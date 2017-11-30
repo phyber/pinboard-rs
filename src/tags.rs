e3ed800756b87a3ca4afb8057a1a37bb8c760363
David O'Rourke <david.orourke@gmail.com>
2017-11-30 01:30:34 +0000

    Return tags as a HashMap<String, String>

--- src/tags.rs
+++ src/tags.rs
@@ -1,12 +1,16 @@
 extern crate serde_json;
 
 use error::CliError;
+use std::collections::HashMap;
 use super::API;
+
+type Tags = HashMap<String, String>;
 
 impl API {
     // Return a list of tags
-    pub fn tags(&self) -> Result<serde_json::Value, CliError> {
+    pub fn tags(&self) -> Result<Tags, CliError> {
         let resp = self.get("tags/get")?;
-        Ok(resp)
+        let tags: Tags = serde_json::from_value(resp)?;
+        Ok(tags)
     }
 }
